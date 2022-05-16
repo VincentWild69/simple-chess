ce25ded6148d8681a8a906efc83661c1246c66b3
Alexey Kuzniecov <alexeyk90@gmail.com>
2022-05-16 17:26:45 +0300

    feat: add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import './styles/App.scss';
-import { changeDataTheme } from './utils/changeDataTheme';
-
+import { Header } from "./Components/Header";
 
 function App() {
-  const onChangeTheme = (theme: string) :void => {
-    changeDataTheme(theme);
-  }
 
   return (
     <div className="container">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={() => onChangeTheme('light')}>light</button>
-        <button onClick={() => onChangeTheme('dark')}>dark</button>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,27 @@
+import { changeDataTheme } from "../utils/changeDataTheme";
+import s from './Header.module.scss';
+import clsx from 'clsx';
+import { GiChessQueen } from 'react-icons/gi';
+import { useState } from "react";
+
+const themesArr: string[] = ['light', 'dark'];
+
+export const Header: React.FC = () => {
+  const [theme, setTheme] = useState<string>('light');
+
+  const onChangeTheme = (theme: string) :void => {
+    changeDataTheme(theme);
+    setTheme(theme);
+  }
+  return (
+    <header className={s.header}>
+      <div className={s.logo}>
+        <GiChessQueen size="3rem" />
+      </div>
+      <div className={s.themeSwitch}>
+        {themesArr.map((themeName: string) => <button key={themeName} className={clsx(s.themeSwitchBtn, {[s.active]: theme === themeName})} onClick={() => onChangeTheme(themeName)}>{themeName}</button>)}
+      </div>
+    </header>
+  )
+
+}
